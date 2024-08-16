eb1aebaed00f2e52a10e63cd5c9eff797e57d7e9
HADI <155739994+HQ-Coding@users.noreply.github.com>
2024-08-16 23:12:42 +0330

    Samurai updated
    slide bug fixed ,drop shadow added, js file updated (Observer) function created for more animation , main folder json file changed

--- Web-Project/Project (Samurai-Soda)/js/script.js
+++ Web-Project/Project (Samurai-Soda)/js/script.js
@@ -1,17 +1,27 @@
+const fromDown = document.querySelectorAll('.fromDown');
+const fromUp = document.querySelectorAll('.fromUp');
+const fromLeft = document.querySelectorAll('.fromLeft');
+const fromRight = document.querySelectorAll('.fromRight');
 
-
-const observer = new IntersectionObserver (entries=>{
-    entries.forEach((entry)=>{
-        entry.target.classList.toggle("show",entry.isIntersecting)
-        if (entry.isIntersecting) observer.unobserve(entry.target)
+const observer = new IntersectionObserver (entries => {
+    entries.forEach( entry =>{
+        entry.target.classList.toggle("show" , entry.isIntersecting)
+        if(entry.IntersectionObserver) observer.unobserve(entry.target)
     }),
     {
-      threshold : .5,
-    }
-  })
+        threshold : .5,
+    }    
+})
 
-const toObserve = ( objList ) =>{
-  objList.forEach(obj=>{
+  fromDown.forEach(obj=>{
     observer.observe(obj)
   })
-}
+  fromUp.forEach(obj=>{
+    observer.observe(obj)
+  })
+  fromLeft.forEach( obj =>{
+    observer.observe(obj)
+  })
+  fromRight.forEach(obj=>{
+    observer.observe(obj)
+  })
