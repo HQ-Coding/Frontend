9fa7621257f28c4505357088dd6c014d2ecfe017
HADI <155739994+HQ-Coding@users.noreply.github.com>
2025-08-20 10:08:26 +0330

    step back to 25

--- base/js/preloader.js
+++ base/js/preloader.js
@@ -6,47 +6,47 @@ setInterval(() => {
   textEl.textContent = "Loading" + ".".repeat(dots);
 }, 500);
 
-// const sectionOne = document.querySelector('.lightAnimation');
-// const step = 40;
-// let count_Of_Lights = Math.round(window.innerWidth / step); 
+const sectionOne = document.querySelector('.lightAnimation');
+const step = 25;
+let count_Of_Lights = Math.round(window.innerWidth / step); 
 
-// function generateSVGPositions(count) {
-//   const points = [];
-//   let x = 0;
-//   const yValues = [100, 120, 140, 160, 140, 120, 100]; 
-//   for (let i = 0; i < count; i++) {
-//     const y = yValues[i % yValues.length];
-//     points.push({ x, y });
-//     x += step;
-//   }
-//   return points;
-// }
+function generateSVGPositions(count) {
+  const points = [];
+  let x = 0;
+  const yValues = [100, 120, 140, 160, 140, 120, 100]; 
+  for (let i = 0; i < count; i++) {
+    const y = yValues[i % yValues.length];
+    points.push({ x, y });
+    x += step;
+  }
+  return points;
+}
 
-// function renderLights() {
-//   sectionOne.innerHTML = ""; 
-//   const light_Positions = generateSVGPositions(count_Of_Lights);
+function renderLights() {
+  sectionOne.innerHTML = ""; 
+  const light_Positions = generateSVGPositions(count_Of_Lights);
 
-//   light_Positions.forEach(({ x, y }, index) => {
-//     const lightSVG = document.createElement('img');
-//     lightSVG.src = 'base/img/anime/black.png';;  
-//     lightSVG.style.position = "absolute";
-//     lightSVG.style.left = x + "px";
-//     lightSVG.style.top = y + "px";
-//     lightSVG.classList.add('lightSVG');
-//     lightSVG.style.transform = "scale(1) translateY(0)";
-//     lightSVG.style.animationDelay = `${index * 0.01}s`;
+  light_Positions.forEach(({ x, y }, index) => {
+    const lightSVG = document.createElement('img');
+    lightSVG.src = 'base/img/anime/black.png';;  
+    lightSVG.style.position = "absolute";
+    lightSVG.style.left = x + "px";
+    lightSVG.style.top = y + "px";
+    lightSVG.classList.add('lightSVG');
+    lightSVG.style.transform = "scale(1) translateY(0)";
+    lightSVG.style.animationDelay = `${index * 0.01}s`;
 
-//     sectionOne.appendChild(lightSVG);
-//   });
-// }
+    sectionOne.appendChild(lightSVG);
+  });
+}
 
 
-// renderLights();
+renderLights();
 
-// window.addEventListener("resize", () => {
-//   count_Of_Lights = Math.round(window.innerWidth / step); 
-//   renderLights();
-// });
+window.addEventListener("resize", () => {
+  count_Of_Lights = Math.round(window.innerWidth / step); 
+  renderLights();
+});
  
 window.addEventListener("load", () => {
     setTimeout(() => {
